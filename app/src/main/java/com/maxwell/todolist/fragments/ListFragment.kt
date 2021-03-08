3e85f71784f710040de9c24c30859ade1f538a34
Max <maxib2007@gmail.com>
2021-03-08 11:41:20 -0300

    adding list and new task view with navigation fragment

---
+++ app/src/main/java/com/maxwell/todolist/fragments/ListFragment.kt
@@ -0,0 +1,27 @@
+package com.maxwell.todolist.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.maxwell.todolist.R
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val v = inflater.inflate(R.layout.fragment_list, container, false)
+
+        v.findViewById<FloatingActionButton>(R.id.fabAddTask).setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return v
+    }
+}
